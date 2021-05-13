295d368ee0009538536cd48c3e7917f22572ff72
yjsyang12 <yjsyang12@126.com>
2021-05-13 22:07:56 +0800

    RESTFUL风格

--- springmvc-03-annotation/src/main/java/com/young/controller/HelloController.java
+++ springmvc-03-annotation/src/main/java/com/young/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.young.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -14,6 +15,13 @@ public class HelloController {
     @RequestMapping("/hello")
     public String hello(Model model) {
         model.addAttribute("msg", "how old are u ?");
+        return "hello";
+    }
+
+    @RequestMapping("/test/{a}/{b}")
+    public String test(@PathVariable int a, @PathVariable int b, Model model) {
+        int result = a + b;
+        model.addAttribute("msg", "结果为" + result);
         return "hello";
     }
 }
--- springmvc-03-annotation/web/WEB-INF/jsp/hello.jsp
+++ springmvc-03-annotation/web/WEB-INF/jsp/hello.jsp
@@ -1,10 +1,3 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: daoben-pc-mi
-  Date: 2021/5/13
-  Time: 16:24
-  To change this template use File | Settings | File Templates.
---%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
